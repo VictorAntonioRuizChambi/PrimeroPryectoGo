c9f2d8ed54bd8b191683fc66fdfe318d39da6b01
Victor Antonio Ruiz Chambi <vruizc@proveedor.intercorp.com.pe>
2022-05-07 13:30:54 -0500

    ejercicios de constantes variables, zero values  e impresión por pantalla

--- src/main.go
+++ src/main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola Mundo!!!")
+
+	// Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.15
+
+	fmt.Println("Este es el valor de pi:", pi)
+	fmt.Println("Este es el valor de pi2:", pi2)
+
+	// Declaracion de variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println("[Base, Altura, Area]: [", base, ", ", altura, ", ", area, "]")
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//Area Cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area del cuadrado:", areaCuadrado)
 }
